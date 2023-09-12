465e35878dbb2571791be59ad9851519470ed12e
Daniel Kim <78152949+dannykim1997@users.noreply.github.com>
2023-09-12 13:50:55 -0700

    Create gameboard and playe objects, add start

---
+++ app.js
@@ -0,0 +1,48 @@
+const GameBoard = (() => {
+    let gameboard = ["", "", "", "", "", "", "", "", ""]
+
+    const render = () => {
+        let boardHTML = "";
+        gameboard.forEach((square, index) => {
+            boardHTML += `<div class="square" id="square-${index}">${square}</div>`
+        })
+        document.querySelector("#gameboard").innerHTML = boardHTML;
+
+    }
+
+    return {
+        render
+    }
+})();
+
+const createPlayer = (name, mark) => {
+    return {
+        name,
+        mark
+    }
+}
+
+const Game = (() => {
+    let players = [];
+    let currentPlayerIndex;
+    let gameOver;
+
+    const start = () => {
+        players = [
+            createPlayer(document.querySelector("#player1").value, "X"),
+            createPlayer(document.querySelector("#player2").value, "O")
+        ]
+        currentPlayerIndex = 0;
+        gameOver = false;
+        GameBoard.render();
+    }
+
+    return {
+        start
+    }
+})();
+
+const startButton = document.querySelector("#start-button");
+startButton.addEventListener("click", () => {
+    Game.start();
+})
